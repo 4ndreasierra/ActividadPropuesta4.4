539f3298ab7f57b222f7bd4e72019def4b408a32
alumno <alumno@linux-vbox-1dam>
2023-02-15 18:19:49 +0100

    Nueva modificacion (2)

--- src/Paquete1/Principal.java
+++ src/Paquete1/Principal.java
@@ -3,5 +3,6 @@ package Paquete1;
 public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Estamos realizando un control de versiones con GIT");
+		System.out.println("Validaciones y comprobaciones");
 	}
 }
