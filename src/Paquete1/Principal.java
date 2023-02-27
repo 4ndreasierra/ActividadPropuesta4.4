820d4bca18e33702b4ba65d465de3d1c82b192bf
alumno <alumno@linux-vbox-1dam>
2023-02-27 16:51:41 +0100

    Subida de datos del  local a remoto

--- src/Paquete1/Principal.java
+++ src/Paquete1/Principal.java
@@ -4,5 +4,6 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Estamos realizando un control de versiones con GIT");
 		System.out.println("Validaciones y comprobaciones");
+		System.out.println("Subida de datos local a remoto");
 	}
 }
